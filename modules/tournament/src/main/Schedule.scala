09ece7e6347de1a1d2bf1d01af1875533a0815de
Isaac Levy <isaac.r.levy@gmail.com>
2016-07-30 19:19:41 -0400

    Add crazyhouse inc tourneys
    Add 1+2 tourneys for half of the SuperBlitz events. For each zh cycle (2x bullet, 1x superblitz, 1x blitz), one of the tourneys will have an increment and the other won't.

--- modules/tournament/src/main/Schedule.scala
+++ modules/tournament/src/main/Schedule.scala
@@ -156,6 +156,7 @@ object Schedule {
 
     (s.speed, s.variant, s.freq) match {
       // Special cases.
+      case (SuperBlitz, Crazyhouse, Hourly) if zhInc(s)    => TC(60, 2)
       case (Blitz, Crazyhouse, Hourly) if zhInc(s)         => TC(4 * 60, 1)
       case (Blitz, Standard, Hourly) if standardInc(s)     => TC(3 * 60, 2)
 
